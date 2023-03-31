ff65114ff5b46b9b067f6f0a094cf88cd0f022b3
mrfret <72273384+mrfret@users.noreply.github.com>
2023-03-31 14:39:24 -0400

    Update bandwithtest.sh

--- bandwithtest.sh
+++ bandwithtest.sh
@@ -15,7 +15,7 @@ testfile='GDSA1:/appbackups/GoogleDriveSpeedTest/500MB.bin'
 # Check if Binfile exists #
 #-------------------------#
 
-if [ ! -f /mnt/remote/Backups/GoogleDriveSpeedTest/500MB.bin ]; then
+if [ ! -f /mnt/remotes/appbackups/GoogleDriveSpeedTest/500MB.bin ]; then
     echo "First run detected. Creating 500MB test file and uploading it to Google Drive."
     mkdir /mnt/remotes/appbackups/GoogleDriveSpeedTest/
     fallocate -l 500M /tmp/500MB.bin
@@ -43,34 +43,34 @@ done
 # Diggity dig dig #
 #-----------------#
 
-mkdir /tmp/tmpapi
-mkdir /tmp/tmpapi/speedresults/
-mkdir /tmp/tmpapi/testfile/
-touch /tmp/tmpapi/rclone.log
-dig +answer $api +short > /tmp/tmpapi/api-ips-fresh
+mkdir tmpapi
+mkdir tmpapi/speedresults/
+mkdir tmpapi/testfile/
+touch tmpapi/rclone.log
+dig +answer $api +short > tmpapi/api-ips-fresh
 
 #--------------------------#
 # Whitelist Known Good IPs #
 #--------------------------#
 
-mv /tmp/tmpapi/api-ips-fresh /tmp/tmpapi/api-ips-progress
+mv tmpapi/api-ips-fresh tmpapi/api-ips-progress
 touch $whitelist
 while IFS= read -r wip; do
-	echo "$wip" >> /tmp/tmpapi/api-ips-progress
+	echo "$wip" >> tmpapi/api-ips-progress
 done < "$whitelist"
-mv /tmp/tmpapi/api-ips-progress /tmp/tmpapi/api-ips-plus-white
+mv tmpapi/api-ips-progress tmpapi/api-ips-plus-white
 
 #------------------------#
 # Backlist Known Bad IPs #
 #------------------------#
 
-mv /tmp/tmpapi/api-ips-plus-white /tmp/tmpapi/api-ips-progress
+mv tmpapi/api-ips-plus-white tmpapi/api-ips-progress
 touch $blacklist
 while IFS= read -r bip; do
-        grep -v "$bip" /tmp/tmpapi/api-ips-progress > /tmp/tmpapi/api-ips
-        mv /tmp/tmpapi/api-ips /tmp/tmpapi/api-ips-progress
+        grep -v "$bip" tmpapi/api-ips-progress > tmpapi/api-ips
+        mv tmpapi/api-ips tmpapi/api-ips-progress
 done < "$blacklist"
-mv /tmp/tmpapi/api-ips-progress /tmp/tmpapi/api-ips
+mv tmpapi/api-ips-progress tmpapi/api-ips
 
 #--------------#
 # Colour codes #
@@ -85,25 +85,25 @@ NC='\033[0m'
 # Checking each IP #
 #------------------#
 
-input=/tmp/tmpapi/api-ips
+input=tmpapi/api-ips
 while IFS= read -r ip; do
 	hostsline="$ip\t$api"
 	sudo -- sh -c -e "echo '$hostsline' >> /etc/hosts"
 	printf "Please wait, downloading the test file from $ip... "
-	rclone copy --log-file /tmp/tmpapi/rclone.log -v "${testfile}" /tmp/tmpapi/testfile
-		if grep -q "KiB/s" /tmp/tmpapi/rclone.log; then
-		speed=$(grep "KiB/s" /tmp/tmpapi/rclone.log | cut -d, -f3 | cut -c 2- | cut -c -5 | tail -1)
+	rclone copy --log-file tmpapi/rclone.log -v "${testfile}" tmpapi/testfile
+		if grep -q "KiB/s" tmpapi/rclone.log; then
+		speed=$(grep "KiB/s" tmpapi/rclone.log | cut -d, -f3 | cut -c 2- | cut -c -5 | tail -1)
 	        printf "${RED}$speed KiB/s${NC} - Blacklisting\n"
-        	rm -r /tmp/tmpapi/testfile
-	        rm /tmp/tmpapi/rclone.log
+        	rm -r tmpapi/testfile
+	        rm tmpapi/rclone.log
 		echo "$ip" >> .blacklist-apis
 		sudo cp /etc/hosts.backup /etc/hosts
 		else
-	speed=$(grep "MiB/s" /tmp/tmpapi/rclone.log | cut -d, -f3 | cut -c 2- | cut -c -5 | tail -1)
+	speed=$(grep "MiB/s" tmpapi/rclone.log | cut -d, -f3 | cut -c 2- | cut -c -5 | tail -1)
 	printf "${GRN}$speed MiB/s${NC}\n"
-	echo "$ip" >> /tmp/tmpapi/speedresults/$speed
-	rm -r /tmp/tmpapi/testfile
-	rm /tmp/tmpapi/rclone.log
+	echo "$ip" >> tmpapi/speedresults/$speed
+	rm -r tmpapi/testfile
+	rm tmpapi/rclone.log
 	sudo cp /etc/hosts.backup /etc/hosts
 	fi
 done < "$input"
@@ -112,9 +112,9 @@ done < "$input"
 # Use best result #
 #-----------------#
 
-ls /tmp/tmpapi/speedresults > /tmp/tmpapi/count
+ls tmpapi/speedresults > tmpapi/count
 max=$(sort -nr /tmp/tmpapi/count | head -1)
-macs=$(cat /tmp/tmpapi/speedresults/$max)
+macs=$(cat tmpapi/speedresults/$max)
 printf "${YEL}The fastest IP is $macs at a speed of $max | putting into hosts file\n"
 hostsline="$macs\t$api"
 sudo -- sh -c -e "echo '$hostsline' >> /etc/hosts"
@@ -123,4 +123,4 @@ sudo -- sh -c -e "echo '$hostsline' >> /etc/hosts"
 # Cleanup tmp files #
 #-------------------#
 
-rm -r /tmp/tmpapi
+rm -r tmpapi
